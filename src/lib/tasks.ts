da9dc0f003b2ec5007dfa12b04d7cf36a10add4f
tuguldurbayar <tuguldur.co@gmail.com>
2025-05-30 17:05:57 +0800

    update

--- src/lib/tasks.ts
+++ src/lib/tasks.ts
@@ -28,15 +28,25 @@ export const baseTasks: Task[] = [
 	{ from: 8, to: 10, text: '📣 Smoke Gank #2: Secure rune/objective', image: './smoke.jpeg' },
 	{ from: 10, to: 12, text: 'Deward high ground, control runes', image: './ward position.jpg' },
 	{ from: 12, to: 14, text: '📣 Smoke Gank #3: Jungle gank carry', image: './smoke.jpeg' },
-	{ from: 14, to: 17, text: 'Reset triangle vision, sentry chokes', image: './' },
-	{ from: 17, to: 20, text: '📣 Smoke Gank #4: Prepare for Roshan', image: './' },
-	{ from: 20, to: 25, text: 'Deep vision, control triangle, secure jungle', image: './' },
-	{ from: 25, to: 30, text: '📣 Smoke Gank #5: Catch enemy split-pushing', image: './' },
-	{ from: 30, to: 35, text: 'Re-establish wards, play safe vision', image: './' },
-	{ from: 35, to: 40, text: '📣 Smoke Gank #6: Set up high-ground siege', image: './' },
-	{ from: 40, to: 45, text: 'Check Roshan, defend triangle', image: './' },
-	{ from: 45, to: 50, text: '📣 Smoke Gank #7: Break stalemate or flank', image: './' },
-	{ from: 50, to: 60, text: 'Secure vision, late-game smokes, reset vision', image: './' }
+	{ from: 14, to: 17, text: 'Reset triangle vision, sentry chokes', image: './ward position.jpg' },
+	{ from: 17, to: 20, text: '📣 Smoke Gank #4: Prepare for Roshan', image: './smoke.jpeg' },
+	{
+		from: 20,
+		to: 25,
+		text: 'Deep vision, control triangle, secure jungle',
+		image: './ward position.jpg'
+	},
+	{ from: 25, to: 30, text: '📣 Smoke Gank #5: Catch enemy split-pushing', image: './smoke.jpeg' },
+	{ from: 30, to: 35, text: 'Re-establish wards, play safe vision', image: './ward position.jpg' },
+	{ from: 35, to: 40, text: '📣 Smoke Gank #6: Set up high-ground siege', image: './smoke.jpeg' },
+	{ from: 40, to: 45, text: 'Check Roshan, defend triangle', image: './ward position.jpg' },
+	{ from: 45, to: 50, text: '📣 Smoke Gank #7: Break stalemate or flank', image: './smoke.jpeg' },
+	{
+		from: 50,
+		to: 60,
+		text: 'Secure vision, late-game smokes, reset vision',
+		image: './ward position.jpg'
+	}
 ];
 
 export const stackingTasks: Task[] = Array.from({ length: 14 }, (_, i) => {
